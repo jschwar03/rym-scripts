819f8e15a6913bde47e26d4ea707e78fc9d65865
sc22j4s <117829431+sc22j4s@users.noreply.github.com>
2024-08-14 14:55:46 +0100

    Setup test

---
+++ full-track-rating.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Full Track Rating
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Specifies tracks to 2 decimal places so you don't need to hover over them
+// @author       jschwar03
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.body.style.backgroundColor = "lightblue";
+})();
