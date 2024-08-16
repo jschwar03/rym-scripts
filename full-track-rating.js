9d19b526110e329140c72157fc7189a14c22d785
sc22j4s <117829431+sc22j4s@users.noreply.github.com>
2024-08-16 18:36:35 +0100

    Update full-track-rating.js
    Worked out HTML elements to grab, but can't access right now

--- full-track-rating.js
+++ full-track-rating.js
@@ -6,11 +6,44 @@
 // @author       jschwar03
 // @match        *://*/*
 // @grant        none
+
 // ==/UserScript==
 
+/*
+* Some extra notes here
+*/
+
 (function() {
-    'use strict';
-    document.body.style.backgroundColor = "lightblue";
+    console.log("Script running");
+    'use strict'; // this means i have to code properly
+
+    // tiptip_content is populated for each track rating
+    let tracks = document.querySelectorAll(".page_artist_songs_song")
+    // output the number of tracks
+
+    tracks.forEach((track) => {
+        let element = track.querySelector(".has_tip.page_artist_songs_song_stats")
+        console.log(element.textContent)
+
+        if (element) {
+            element.dispatchEvent(new MouseEvent("mouseover", { bubbles: true }));
+
+            setTimeout(() => {
+                let test = document.querySelector(".tiptip_content")
+                console.log(test)
+                let fullRating = document.querySelector(".tiptip_content").textContent.trim().split(' ')[0];
+                
+                element.textContent = fullRating;
+            }, 500)
+        }
+    });
+    console.log(tracks.length);
+    let ratings = document.querySelectorAll(".tiptip_content");
+    
+
+    // update the rating 
+    
+
+    
 })();
 
-fd**
